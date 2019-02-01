d1d540aecd2f40538235dabd9b2ee64766b55dd6
zrheaume <zachary.a.rheaume@gmail.com>
2019-02-01 10:40:01 -0500

    Made coilerplate better

--- assets/app/Scraper.js
+++ assets/app/Scraper.js
@@ -1,3 +1,11 @@
+const cheerio = require("cheerio")
+const axios = require("axios")
 module.exports = class Scraper {
-    
+    constructor(URL) {
+        this.target = URL
+    }
+    async getDocument() {
+        let response = await axios.get(URL)
+        this.document = response.data
+    }
 }
